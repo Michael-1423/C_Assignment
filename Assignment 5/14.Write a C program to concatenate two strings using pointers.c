7a4e26a8eb39ba4a6a70d9f0430ccb6bc7806b2f
Michael-1423 <95876418+Michael-1423@users.noreply.github.com>
2022-01-01 23:14:40 +0530

    Update 14.Write a C program to concatenate two strings using pointers.c

--- Assignment 5/14.Write a C program to concatenate two strings using pointers.c
+++ Assignment 5/14.Write a C program to concatenate two strings using pointers.c
@@ -1,41 +1,25 @@
 //14.Write a C program to concatenate two strings using pointers.
 #include <stdio.h>
 
-int main(){
-    
-    int a;
-    char *ptr1, *ptr2, *ptr3;
-    
-    printf("Enter the size of the string\n");
-    scanf("%d", &a);
-    char str1[a+1], str2[a+1], str3[(2*a)+1];
-    
-    printf("Enter the first string\n");
-    
-    ptr1 = str1;
-    scanf("%s", ptr1);
-    
-    printf("Enter the second string\n");
-    
-    ptr2 = str2;
-    scanf("%s", ptr2);
-    
-    ptr3 = str3;
-    
-    for(int i=0; i<=(2*a); i++){
-        if(i<a){
-            *(ptr3 + i) = *(ptr1 + i);
-            printf("working");
-        }else{
-            *(ptr3 + i) = *(ptr2 + i);
-            printf("working");
-        }
+int main()
+{
+    char str1[20],str2[20],str3[40],*p1,*p2,*p3;
+    printf("Enter string 1 ");
+    gets(str1);
+    printf("Enter string 2 ");
+    gets(str2);
+    p1=str1;
+    p2=str2;
+    while(*p1){
+        p1++;
     }
-    *(ptr3 + 2*a+1) = '\0';
-    
-    printf("The concatinated string is\n");
-    
-    for(int i=0; i<(2*a); i++){
-        printf("%c", *(ptr3+i));
+    while(*p2){
+        *p1 = *p2;
+        p2++;
+        p1++;
     }
+    *p1='\0';
+    printf("the concatinated string is %s",str1);
+    return 0;
+    
 }
